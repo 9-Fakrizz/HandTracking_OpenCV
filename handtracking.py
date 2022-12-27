5a7cb897cf84202cfd96e94abc9c6985c6b72519
suphakrit <suphakrit_kongkham@cmu.ac.th>
2022-12-27 19:19:57 +0700

    I put my comments for every you!

--- handtracking.py
+++ handtracking.py
@@ -12,7 +12,7 @@ mpDraw = mp.solutions.drawing_utils
 
 pTime = 0
 cTime = 0
-pos = numpy.zeros((21, 2))
+pos = numpy.zeros((21, 2)) #using numpy arrays
 side = "none"
 TipsID = [8, 12, 16, 20]
 Pip = [6, 10, 14, 18]
@@ -32,36 +32,40 @@ while True:
             for id, lm in enumerate(handLms.landmark): #get id and position
                 h, w, c = img.shape
                 cx, cy = int(lm.x*w), int(lm.y*h)
-                pos[id] = [cx, cy]
+                pos[id] = [cx, cy]  #get each position of id into pos array
             
             #print(pos[0][0], pos[0][1])
+           
             thdx = mt.pow(pos[4, 0]-pos[17, 0],2)
             thdy = mt.pow(pos[4, 1]-pos[17, 1],2)
-            Lthumb = int(mt.sqrt(abs(thdy + thdx))) #thumb distancing
+            Lthumb = int(mt.sqrt(abs(thdy + thdx))) #thumb1 is distancing from id 4 to id 17
             
-            th0dx = mt.pow(pos[3, 0]-pos[17, 0],2)
+            th0dx = mt.pow(pos[3, 0]-pos[17, 0],2)  
             th0dy = mt.pow(pos[3, 1]-pos[17, 1],2)
-            Lthumb0 = int(mt.sqrt(abs(th0dy + th0dx)))
+            Lthumb0 = int(mt.sqrt(abs(th0dy + th0dx)))  #thumb0 is distancing from id 3 to id 17
             
-            long1= numpy.zeros((4, 1))
+            long1= numpy.zeros((4, 1)) #create long0 & long1 every finger exept thumb
             long0= numpy.zeros((4, 1))
             for i in range(0, 4):
                 dx1 = mt.pow(pos[TipsID[i], 0]-pos[0, 0],2)
                 dy1 = mt.pow(pos[TipsID[i], 1]-pos[0, 1],2)
-                long1[i] = int(mt.sqrt(abs(dy1 + dx1))) 
+                long1[i] = int(mt.sqrt(abs(dy1 + dx1)))  #long1 are distancing from tip of finger to 0 
 
                 dx0 = mt.pow(pos[Pip[i], 0]-pos[0, 0],2)
                 dy0 = mt.pow(pos[Pip[i], 1]-pos[0, 1],2)
-                long0[i] = int(mt.sqrt(abs(dy0 + dx0)))
-            
+                long0[i] = int(mt.sqrt(abs(dy0 + dx0))) #long1 are distancing from pip of finger to 0
+            #now we already have long0 & long1 of every finger
+            #next step we will compare long1 to long0
+
         finger=[]
         if (Lthumb > Lthumb0):
-            finger.append(constfinger[0])
-        for i in range(0, 4):
+            finger.append(constfinger[0]) #condition for thumb
+        
+        for i in range(0, 4): #condition for another finger
             if (long1[i] > long0[i]):
                 finger.append(constfinger[i+1])
             
-            #print(finger, len(finger))
+        #print(finger, len(finger))
             
         mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
         if(len(finger)!=0):
